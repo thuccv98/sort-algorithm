549642748cbed6aa02cd69ff1747f46d717086bd
thuccv98 <thuccv98@gmail.com>
2021-08-14 10:48:22 +0700

    Add README file

--- bubble-sort/BubbleSort.js
+++ bubble-sort/BubbleSort.js
@@ -1,19 +1,19 @@
 // Buble sort - sắp xếp nổi bọt
 let bubbleSort = (arr) => {
-	// Độ dài của mảng
-	let n = arr.length;
+  // Độ dài của mảng
+  let n = arr.length;
 
-	// Logic
-	for(let i = 0; i < n - 1; i++){
-		for(let j = 0; j <= n - i - 1; j++){
-			// Swap the numbers
-			if(arr[j] > arr[j+1]){
-				[arr[j], arr[j+1]] = [arr[j+1], arr[j]];
-			}
-		}
-	}
+  // Logic
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      // Swap the numbers
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
 
-	return arr;
-}
+  return arr;
+};
 
-console.log(bubbleSort([5, 8, 3, -8, 9, 0]));
+console.log(bubbleSort([5, 8, 3, -8, 9, 0, 5, 56]));
